f1f61f7781af27e64bdb6ecd5effee80adad6d5b
ManuCas <manucastle@hotmail.com>
2019-04-14 19:35:36 -0500

    Frase arrays added, firebase conection completed

--- assets/javascript/java.js
+++ assets/javascript/java.js
@@ -1,12 +1,58 @@
+//initial firebase app data
+var config = {
+    apiKey: "AIzaSyAR4AE4vZ8gZqP46m9Wj1ZOND4d8TaZ4Iw",
+    authDomain: "project-1-boot-camp.firebaseapp.com",
+    databaseURL: "https://project-1-boot-camp.firebaseio.com",
+    projectId: "project-1-boot-camp",
+    storageBucket: "project-1-boot-camp.appspot.com",
+    messagingSenderId: "523831754951"
+};
+
+//initialize firebase app
+firebase.initializeApp(config);
+//firebase var
+var firedB = firebase.database();
+
 var key = "55c220cb58mshee0f3639dbd0c90p1bb460jsn121fe9054ac1";
 var keyFun = "test";
+var yodaArr = ["yoda", "Jedi", "Dagobah", "The_Force", "Object–subject–verb"];
+var sithArr = ["Darth_Maul","Darth_Vader","Darth Maul: Shadow Hunter","Might_makes_right"];
+var klingonArr = ["klingon","Kahless", "The_Klingon_Dictionary"];
+var pirateArr = ["Privateer","Commerce_raiding","Ahoy_(greeting)"];
+var minionArr = ["Minions_(Despicable_Me)", "Minions_Paradise", "Minions_(film)", "Minions_(Despicable_Me)"];
+var gunganArr = ["List_of_Star_Wars_species_(F–J)#Gungan", "Jar_Jar_Binks", "Naboo"];
+var ferbArr = ["Ferb_Fletcher", "Phineas_and_Ferb", "Phineas_Flynn", "Perry_the_Platypus"];
+var morseArr = ["Morse_code", "Samuel_Morse", "Character_encoding", "Telegraphy", "SOS"];
 
+//Generte translation stats for Stats modal
+$("#statsbtn").click(function (event) {
+    event.preventDefault();
+    //get counts data
+    firedB.ref("/counters").on("value", function (snapshot) {
+        var yodaStats = snapshot.val().yoda;
+        var sithStats = snapshot.val().sith;
+        var klingonStats = snapshot.val().klingon;
+        var pirateStats = snapshot.val().pirate;
+        var minionStats = snapshot.val().minion;
+        var gunganStats = snapshot.val().gungan;
+        var ferbStats = snapshot.val().ferb;
+        var morseStats = snapshot.val().morse;
+        $("#yodastats").text(yodaStats + " Traslations");
+        $("#sithstats").text(sithStats + " Traslations");
+        $("#klingonstats").text(klingonStats + " Traslations");
+        $("#piratestats").text(pirateStats + " Traslations");
+        $("#minionstats").text(minionStats + " Traslations");
+        $("#gunganstats").text(gunganStats + " Traslations");
+        $("#ferbstats").text(ferbStats + " Traslations");
+        $("#morsestats").text(morseStats + " Traslations");
+    }, function (errorObject) {
+        // In case of error this will print the error
+        console.log("The read failed: " + errorObject.code);
+    });
+});
 
 $("#newbtn").click(function (event) {
     event.preventDefault();
-    //generate random to retrieve wiki frase
-    var x = Math.floor((Math.random() * 3));
-    console.log(x);
     $("#error").empty();
     $("#trans-text").val("");
     $("html").css({ "background-color": "black", "background-image": "url(assets/images/fiction.jpg)" });
@@ -17,60 +63,56 @@ $("#newbtn").click(function (event) {
     $("#trans-h3").empty();
     $("#fact-h1").empty();
     $("#fact-h3").empty();
-
 });
 
 $("#yodabtn").click(function (event) {
     event.preventDefault();
-    setTimeout(yodaCall(), 1500);
+    yodaCall();
 });
 
 $("#sithbtn").click(function (event) {
     event.preventDefault();
-    setTimeout(sithCall(), 1500);
+    sithCall();
 });
 
 $("#klinbtn").click(function (event) {
     event.preventDefault();
-    setTimeout(klingonCall(), 1500);
+    klingonCall();
 });
 
 $("#piratebtn").click(function (event) {
     event.preventDefault();
-    setTimeout(pirateCall(), 1500);
+    pirateCall();
 });
 
 
 $("#minionbtn").click(function (event) {
     event.preventDefault();
-    setTimeout( minionCall(), 1500);
+    minionCall();
 });
 
 $("#gunganbtn").click(function (event) {
     event.preventDefault();
-    setTimeout(gunganCall(), 1500);
+    gunganCall();
 });
 
 
 $("#ferbbtn").click(function (event) {
     event.preventDefault();
-    setTimeout(ferbCall(), 1500);
+    ferbCall();
 });
 
 $("#morsebtn").click(function (event) {
     event.preventDefault();
-    setTimeout(morseCall(), 1500);
+    morseCall();
 });
 
 //app  code's functions
 function renderTranslation(result) {
-    $('#translationModal').modal('toggle');
-    $("#jumbotron").hide();
     $("#original-h1").text("Your text was:");
     $("#original-h3").text(result.contents.text);
     $("#trans-h1").text('In "' + result.contents.translation + '" You would say:');
     $("#trans-h3").text(result.contents.translated);
-
 }
 
 function facts(frase) {
@@ -96,7 +138,6 @@ function facts(frase) {
 
         error: function (errorMessage) {
         }
-
     });
     ///back to top
     setTimeout(function () {
@@ -104,13 +145,20 @@ function facts(frase) {
     }, 1000);
 }
 
+function eraseIndex() {
+    $('#translationModal').modal('toggle');
+    $("#jumbotron").hide();
+    return;
+}
+
 function yodaCall() {
     var newInput = $("#trans-text").val().trim();
     if (newInput == "") {
         $("#validation-text").text("Type something please");
-        setTimeout(function () { $("#validation-text").empty(); }, 2000);
+        setTimeout(function () { $("#validation-text").empty() }, 2000);
     }
     else {
+        eraseIndex();
         // Yoda translations
         $("html").css({ "background-color": "black", "background-image": "url(assets/images/yodabg.jpg)" });
         $.ajax({
@@ -123,25 +171,32 @@ function yodaCall() {
                 'header1': 'header-value-1'
             },
         }).then(function (translation) {
-            console.log(translation);
             console.log(translation.contents);
-
             renderTranslation(translation);
-             //wikipwdia
-        var frase = "yoda"
-        facts(frase);
+             //generate random to retrieve wiki frase
+            var x = Math.floor((Math.random() * yodaArr.length));
+            console.log(x);
+            var frase = yodaArr[x];
+            console.log("wiki Frase: " + frase);
+            //wikipwdia
+            facts(frase);
+            // Increment firebase yoda counter by 1.
+            var count = firedB.ref('counters/yoda');
+            count.transaction(function (currentCount) {
+                return currentCount + 1;
+            });
         });
     }
-
 }
 
 function sithCall() {
     var newInput = $("#trans-text").val().trim();
     if (newInput == "") {
         $("#validation-text").text("Type something please");
-        setTimeout(function () { $("#validation-text").empty(); }, 2000);
+        setTimeout(function () { $("#validation-text").empty() }, 2000);
     }
     else {
+        eraseIndex();
         // siith translations
         $("html").css({ "background-color": "black", "background-image": "url(assets/images/sithbg.jpg)" });
         $.ajax({
@@ -154,24 +209,33 @@ function sithCall() {
                 'header1': 'header-value-1'
             },
         }).then(function (translation) {
-            console.log(translation);
             console.log(translation.contents);
-            // console.log("length"+JSON.stringify(translation.success.total));
             renderTranslation(translation);
-            //wikipwdia
-        var frase = "Darth Maul: Shadow Hunter"
-        facts(frase);
+             //generate random to retrieve wiki frase
+             var x = Math.floor((Math.random() * sithArr.length));
+             console.log(x);
+             var frase = sithArr[x];
+             console.log("wiki Frase: " + frase);
+             //wikipwdia
+             facts(frase);
+            // Increment firebase sith counter by 1.
+            var count = firedB.ref('counters/sith');
+            count.transaction(function (currentCount) {
+                return currentCount + 1;
+            });
         });
     }
 }
 
 function klingonCall() {
+
     var newInput = $("#trans-text").val().trim();
     if (newInput == "") {
         $("#validation-text").text("Type something please");
-        setTimeout(function () { $("#validation-text").empty(); }, 2000);
+        setTimeout(function () { $("#validation-text").empty() }, 2000);
     }
     else {
+        eraseIndex();
         // kinglon translations
         $("html").css({ "background-color": "black", "background-image": "url(assets/images/klingonbg.jpg)" });
         $.ajax({
@@ -183,12 +247,20 @@ function klingonCall() {
                 "X-FunTranslations-Api-Secret": keyFun,
             },
         }).then(function (translation) {
-            console.log(translation);
             console.log(translation.contents);
             renderTranslation(translation);
+             //generate random to retrieve wiki frase
+             var x = Math.floor((Math.random() * klingonArr.length));
+             console.log(x);
+             var frase = klingonArr[x];
+             console.log("wiki Frase: " + frase);
              //wikipwdia
-        var frase = "Klingon"
-        facts(frase);
+             facts(frase);
+            // Increment firebase klingon counter by 1.
+            var count = firedB.ref('counters/klingon');
+            count.transaction(function (currentCount) {
+                return currentCount + 1;
+            });
         }).catch(err => {
             $("#error").text(err.responseJSON.error.message + " Or try another language");
         });
@@ -199,9 +271,10 @@ function pirateCall() {
     var newInput = $("#trans-text").val().trim();
     if (newInput == "") {
         $("#validation-text").text("Type something please");
-        setTimeout(function () { $("#validation-text").empty(); }, 2000);
+        setTimeout(function () { $("#validation-text").empty() }, 2000);
     }
     else {
+        eraseIndex();
         // Pirate translations
         $("html").css({ "background-color": "black", "background-image": "url(assets/images/piratebg.jpg)" });
         $.ajax({
@@ -213,12 +286,20 @@ function pirateCall() {
                 'X-RapidAPI-Key': key,
             },
         }).then(function (translation) {
-            console.log(translation);
             console.log(translation.contents);
             renderTranslation(translation);
-              //wikipwdia
-        var frase = "Ahoy_(greeting)"
-        facts(frase);
+             //generate random to retrieve wiki frase
+             var x = Math.floor((Math.random() * pirateArr.length));
+             console.log(x);
+             var frase = pirateArr[x];
+             console.log("wiki Frase: " + frase);
+             //wikipwdia
+             facts(frase);
+            // Increment firebase pirate counter by 1.
+            var count = firedB.ref('counters/pirate');
+            count.transaction(function (currentCount) {
+                return currentCount + 1;
+            });
         });
     }
 }
@@ -227,9 +308,10 @@ function minionCall() {
     var newInput = $("#trans-text").val().trim();
     if (newInput == "") {
         $("#validation-text").text("Type something please");
-        setTimeout(function () { $("#validation-text").empty(); }, 2000);
+        setTimeout(function () { $("#validation-text").empty() }, 2000);
     }
     else {
+        eraseIndex();
         // Minion translations
         $("html").css({ "background-color": "black", "background-image": "url(assets/images/minionbg.jpg)" });
         $.ajax({
@@ -240,15 +322,22 @@ function minionCall() {
                 'Content-Type': 'application/x-www-form-urlencoded',
                 "X-RapidAPI-Host": "minion.https://rapidapi.p.rapidapi.com",
                 'X-RapidAPI-Key': key,
-
             },
         }).then(function (translation) {
-            console.log(translation);
             console.log(translation.contents);
             renderTranslation(translation);
-              //wikipwdia
-        var frase = "Minions_(film)"
-        facts(frase);
+             //generate random to retrieve wiki frase
+             var x = Math.floor((Math.random() * minionArr.length));
+             console.log(x);
+             var frase = minionArr[x];
+             console.log("wiki Frase: " + frase);
+             //wikipwdia
+             facts(frase);
+            // Increment firebase minion counter by 1.
+            var count = firedB.ref('counters/minion');
+            count.transaction(function (currentCount) {
+                return currentCount + 1;
+            });
         });
     }
 }
@@ -258,9 +347,10 @@ function gunganCall() {
     var newInput = $("#trans-text").val().trim();
     if (newInput == "") {
         $("#validation-text").text("Type something please");
-        setTimeout(function () { $("#validation-text").empty(); }, 2000);
+        setTimeout(function () { $("#validation-text").empty() }, 2000);
     }
     else {
+        eraseIndex();
         // Gungan translations
         $("html").css({ "background-color": "black", "background-image": "url(assets/images/gunganbg.jpg)" });
         $.ajax({
@@ -271,12 +361,20 @@ function gunganCall() {
                 'X-RapidAPI-Key': key,
             },
         }).then(function (translation) {
-            console.log(translation);
             console.log(translation.contents);
             renderTranslation(translation);
+            //generate random to retrieve wiki frase
+            var x = Math.floor((Math.random() * gunganArr.length));
+            console.log(x);
+            var frase = gunganArr[x];
+            console.log("wiki Frase: " + frase);
             //wikipwdia
-        var frase = "Jar_Jar_Binks"
-        facts(frase);
+            facts(frase);
+            // Increment firebase gungan counter by 1.
+            var count = firedB.ref('counters/gungan');
+            count.transaction(function (currentCount) {
+                return currentCount + 1;
+            });
         });
     }
 }
@@ -286,9 +384,10 @@ function ferbCall() {
     var newInput = $("#trans-text").val().trim();
     if (newInput == "") {
         $("#validation-text").text("Type something please");
-        setTimeout(function () { $("#validation-text").empty(); }, 2000);
+        setTimeout(function () { $("#validation-text").empty() }, 2000);
     }
     else {
+        eraseIndex();
         // FerbLatin translations
         $("html").css({ "background-color": "black", "background-image": "url(assets/images/ferbbg.jpg)" });
         $.ajax({
@@ -299,12 +398,20 @@ function ferbCall() {
                 'X-RapidAPI-Key': key,
             },
         }).then(function (translation) {
-            console.log(translation);
             console.log(translation.contents);
             renderTranslation(translation);
-            //wikipwdia
-        var frase = "Phineas_and_Ferb"
-        facts(frase);
+             //generate random to retrieve wiki frase
+             var x = Math.floor((Math.random() * ferbArr.length));
+             console.log(x);
+             var frase = ferbArr[x];
+             console.log("wiki Frase: " + frase);
+             //wikipwdia
+             facts(frase);
+            // Increment firebase ferb counter by 1.
+            var count = firedB.ref('counters/ferb');
+            count.transaction(function (currentCount) {
+                return currentCount + 1;
+            });
         });
     }
 }
@@ -313,9 +420,10 @@ function morseCall() {
     var newInput = $("#trans-text").val().trim();
     if (newInput == "") {
         $("#validation-text").text("Type something please");
-        setTimeout(function () { $("#validation-text").empty(); }, 2000);
+        setTimeout(function () { $("#validation-text").empty() }, 2000);
     }
     else {
+        eraseIndex();
         // Morse code translations
         $("html").css({ "background-color": "black", "background-image": "url(assets/images/morsebg.jpg)" });
         $.ajax({
@@ -328,12 +436,20 @@ function morseCall() {
                 "X-FunTranslations-Api-Secret": keyFun,
             },
         }).then(function (translation) {
-            console.log(translation);
             console.log(translation.contents);
             renderTranslation(translation);
+             //generate random to retrieve wiki frase
+             var x = Math.floor((Math.random() * morseArr.length));
+             console.log(x);
+             var frase = morseArr[x];
+             console.log("wiki Frase: " + frase);
              //wikipwdia
-            var frase = "morse code"
-        facts(frase);
+             facts(frase);
+            // Increment firebase morse counter by 1.
+            var count = firedB.ref('counters/morse');
+            count.transaction(function (currentCount) {
+                return currentCount + 1;
+            });
         }).catch(err => {
             $("#error").text(err.responseJSON.error.message + " Or try another language");
         });
